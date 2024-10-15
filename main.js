52daf45621012d1a73c95e70abd53b0de205b714
Arinaho muleba <88719882+AMstyles@users.noreply.github.com>
2024-10-15 16:14:10 +0200

    direct link

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw.js')
+        navigator.serviceWorker.register('https://amstyles.github.io/web-image-optimisation-sample/sw.js')
             .then((registration) => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
